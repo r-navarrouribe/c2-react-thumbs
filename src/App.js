71763162add96d48a7fc0e41f50b35f5bea97089
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-06-10 21:27:32 +0200

    Ejercicio terminado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
+import meGusta from "./img/me-gusta.png";
+import noMeGusta from "./img/no-me-gusta.jpg";
+
 function App() {
+  const [thumb, setThumb] = useState(true);
+  const changeThumb = () => {
+    setThumb(thumb ? false : true);
+  };
   return (
-    <div className="App">
-      <p>holi</p>
-    </div>
+    <>
+      <div className="contenedor">
+        <i
+          className={thumb ? "far fa-thumbs-up" : "far fa-thumbs-down"}
+          onClick={changeThumb}
+        ></i>
+        <img
+          src={thumb ? meGusta : noMeGusta}
+          alt={thumb ? "Cara de satisfacción" : "Cara de desagrado"}
+        />
+      </div>
+    </>
   );
 }
 
